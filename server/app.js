a454d50c319e21abe9192e5fe5e282af0c3aeac8
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2025-10-01 20:18:57 +0700

    update server.js

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.use(
     crossOriginEmbedderPolicy: false,
   })
 );
-app.use(cors());
+app.use(cors({ origin: "https://sendo-snowy.vercel.app" }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
